a482e81685dd73a2e411d1f901e4cd8c0eff77ae
Kirill Smetankin <smet_k@MacBook-Pro-Kirill.local>
2018-10-21 16:38:27 +0300

    some changes

--- app/src/main/java/com/example/smet_k/bauman_gis/MainActivity.java
+++ app/src/main/java/com/example/smet_k/bauman_gis/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.smet_k.bauman_gis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ImageButton startNewActivityBtn = findViewById(R.id.imageButton);
+
+        startNewActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent navIntent = new Intent(MainActivity.this, NavigatorActivity.class);
+
+                startActivity(navIntent);
+            }
+        });
     }
+
+
 }
