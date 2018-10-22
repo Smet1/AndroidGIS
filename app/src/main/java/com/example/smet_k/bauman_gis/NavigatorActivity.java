03aa882b53cab512a41730e159255d16fa689495
Kirill Smetankin <smet_k@MacBook-Pro-Kirill.local>
2018-10-22 13:24:44 +0300

    added routeFragment

--- app/src/main/java/com/example/smet_k/bauman_gis/NavigatorActivity.java
+++ app/src/main/java/com/example/smet_k/bauman_gis/NavigatorActivity.java
@@ -1,9 +1,14 @@
 package com.example.smet_k.bauman_gis;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class NavigatorActivity extends AppCompatActivity {
     @Override
@@ -11,5 +16,30 @@ public class NavigatorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigator);
 
+        final Button startNewActivityBtn = findViewById(R.id.Calculate);
+
+        startNewActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleState();
+            }
+        });
+
+    }
+    private void toggleState() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        Fragment bottom = getSupportFragmentManager().findFragmentById(R.id.TopFrame);
+
+        if (bottom != null && bottom.isAdded()) {
+            transaction.remove(bottom);
+            transaction.add(R.id.TopFrame, new RouteFragment());
+        } else {
+            transaction.add(R.id.TopFrame, new RouteFragment());
+        }
+
+
+
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/smet_k/bauman_gis/RouteFragment.java
@@ -0,0 +1,17 @@
+package com.example.smet_k.bauman_gis;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RouteFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_blue, container, false);
+    }
+}
