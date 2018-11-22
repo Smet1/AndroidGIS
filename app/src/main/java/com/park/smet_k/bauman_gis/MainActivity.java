8cfd59b281d27714ff59e12b3269bf6a2f9b71e8
smet_k <ski97@mail.ru>
2018-11-22 21:07:51 +0300

    some changes

--- app/src/main/java/com/park/smet_k/bauman_gis/MainActivity.java
+++ app/src/main/java/com/park/smet_k/bauman_gis/MainActivity.java
@@ -1,19 +1,33 @@
 package com.park.smet_k.bauman_gis;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final ImageButton startNewActivityBtn = findViewById(R.id.imageButton);
+
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         startNewActivityBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +39,13 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
 
