7840c5bf941d635a8f10cb495a49790a2835802c
smet_k <ski97@mail.ru>
2018-12-25 03:11:49 +0300

    design update

--- app/src/main/java/com/park/smet_k/bauman_gis/MainActivity.java
+++ app/src/main/java/com/park/smet_k/bauman_gis/MainActivity.java
@@ -2,18 +2,15 @@ package com.park.smet_k.bauman_gis;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.telecom.Call;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -154,6 +151,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
+            case R.id.home: {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.container, ServerNewsFragment.newInstance())
+                        .addToBackStack(null)
+                        .commit();
+
+                drawerLayout.closeDrawers();
+                break;
+            }
+
             case R.id.account: {
                 getSupportFragmentManager().beginTransaction()
                         .replace(R.id.container, AccountFragment.newInstance())
---
+++ app/src/main/java/com/park/smet_k/bauman_gis/ServerNewsFragment.java
@@ -0,0 +1,27 @@
+package com.park.smet_k.bauman_gis;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ServerNewsFragment extends Fragment {
+
+    public static ServerNewsFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        ServerNewsFragment fragment = new ServerNewsFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.server_news_fragment, container, false);
+    }
+}
