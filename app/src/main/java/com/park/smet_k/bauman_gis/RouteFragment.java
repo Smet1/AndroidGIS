6865bcc62f34e01ac9de03c23ffd7c223f7730f7
smet_k <ski97@mail.ru>
2019-05-04 12:42:29 +0300

    detecting points on same level

--- app/src/main/java/com/park/smet_k/bauman_gis/RouteFragment.java
+++ app/src/main/java/com/park/smet_k/bauman_gis/RouteFragment.java
@@ -6,15 +6,13 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.park.smet_k.bauman_gis.model.StairsLink;
-
 import java.util.ArrayList;
-import java.util.List;
 
 public class RouteFragment extends Fragment {
     // просчет маршрута при создании фрагмента
@@ -50,7 +48,6 @@ public class RouteFragment extends Fragment {
         if (args != null) {
             from = args.getInt("from");
             to = args.getInt("to");
-
         }
 
         TextView textView = view.findViewById(R.id.from);
@@ -59,34 +56,27 @@ public class RouteFragment extends Fragment {
         textView = view.findViewById(R.id.to);
         textView.setText(to.toString());
 
-
-        // TODO(): размер графа и количество связей
-        WeightedGraph graph = new WeightedGraph(AppComponent.getInstance().StairsArray.size(),
-                AppComponent.getInstance().StairsLinksArray.size());
-
-        // TODO(): не генерить граф здесь
-//        List<StairsLink> links = AppComponent.getInstance().StairsLinksArray;
-//        for (StairsLink val : links) {
-//            // id в бд начинаются с 1
-//
-//            // проверка на валидность связи (открыто или нет)
-//            if (val.getOpen()) {
-//                graph.addEdge(val.getIdFrom() - 1, val.getIdTo() - 1, val.getWeight());
-//            }
-//        }
-
-
         // фиксируем прибыль (уличная магия)
         ArrayList<Integer> route = AppComponent.getInstance().StairsGraph.dijkstra(from - 1, to - 1);
 
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for (Integer val : route) {
             val += 1;
-            result += (", " + val.toString());
+            result.append(", ").append(val.toString());
+        }
+
+        result.append("|");
+
+        for (int i = 0; i < route.size() - 1; i++) {
+            result.append("run A star on: ");
+            if (AppComponent.getInstance().StairsArray.get(route.get(i)).getLevel().equals(AppComponent.getInstance().StairsArray.get(route.get(i + 1)).getLevel())) {
+                Log.d("kek", "same level, run A star on " + String.valueOf(route.get(i) + 1) + " " + String.valueOf(route.get(i + 1) + 1));
+                result.append(route.get(i)).append(" ").append(route.get(i) + 1).append(", ");
+            }
         }
 
         TextView routeView = view.findViewById(R.id.route);
-        routeView.setText(result);
+        routeView.setText(result.toString());
     }
 }
 
